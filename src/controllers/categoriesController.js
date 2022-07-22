54ceb7975c4ba0c7daaa6c6f7b2498d221aaf1ad
pgeovany <pgeovany@ic.ufrj.br>
2022-07-22 17:24:20 -0300

    feat: implement get categories route

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -1,7 +1,16 @@
+import connection from '../databases/postgresql.js';
 import STATUS from '../utils/statusCodes.js';
 
 async function getCategories(req, res) {
-  res.sendStatus(STATUS.OK);
+  try {
+    const { rows: categories } = await connection.query(`
+      SELECT * FROM categories
+    `);
+
+    res.send(categories);
+  } catch (error) {
+    res.sendStatus(STATUS.INTERNAL_SERVER_ERROR);
+  }
 }
 
 async function addCategory(req, res) {
