10c0ab0af243c155f995c135d00657e6b1d9be2d
pgeovany <pgeovany@ic.ufrj.br>
2022-07-23 17:29:50 -0300

    feat: implement a route to get the list of customers

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -1,7 +1,21 @@
 import STATUS from '../utils/statusCodes.js';
+import connection from '../databases/postgresql.js';
 
 async function getCustomers(req, res) {
-  res.sendStatus(STATUS.OK);
+  const { cpf } = req.query;
+  try {
+    let query = `SELECT * FROM customers`;
+
+    if (cpf) {
+      query += ` WHERE cpf ILIKE '${cpf}%'`;
+    }
+
+    const { rows: customers } = await connection.query(query);
+
+    res.send(customers);
+  } catch (error) {
+    res.sendStatus(STATUS.INTERNAL_SERVER_ERROR);
+  }
 }
 
 async function getCustomerById(req, res) {
